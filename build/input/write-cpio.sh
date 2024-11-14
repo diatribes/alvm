9644048d4a89e951510a514e3fde8c5630206154
diatribes@gmail.com <diatribes@gmail.com>
2024-11-14 15:38:32 +0000

    do not cpio /mnt

--- build/input/write-cpio.sh
+++ build/input/write-cpio.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-/bin/busybox find / ! -path "/sys/*" ! -path "/proc/*" ! -path "/dev/*" \
+/bin/busybox find / ! -path "/mnt/*" ! -path "/sys/*" ! -path "/proc/*" ! -path "/dev/*" \
  | cpio -o -H newc > /mnt/output/rootfs-new.cpio
 
