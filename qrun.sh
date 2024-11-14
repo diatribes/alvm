9507d4abccd26e55e258b5c4d0a7a146e6eac514
diatribes <diatribes@gmail.com>
2024-11-14 16:21:34 -0500

    bugfix

--- qrun.sh
+++ qrun.sh
@@ -13,7 +13,7 @@ qemu-system-x86_64 \
     -serial none -device isa-serial,chardev=s1 \
     -chardev stdio,id=s1 \
     -append "panic=-1 notsc" \
-    -initrd "${OUTPUTPATH}"/rootfs-new.cpio \
+    -initrd "${OUTPUTPATH}"/rootfs.cpio \
     -nic user,model=virtio-net-pci \
     -virtfs local,path=${INPUTPATH},mount_tag=host0,security_model=none,id=host0 \
     -virtfs local,path=${OUTPUTPATH},mount_tag=host1,security_model=none,id=host1
