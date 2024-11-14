300d16beefb34aec5007df8ac40fc37698ccf39f
diatribes <diatribes@gmail.com>
2024-11-14 16:20:52 -0500

    enable kvm and host cpu

--- qrun.sh
+++ qrun.sh
@@ -5,13 +5,15 @@ qemu-system-x86_64 \
     -no-reboot \
     -no-user-config \
     -nodefaults \
+    -cpu host \
+    -enable-kvm \
     -m 256 \
     -kernel "${OUTPUTPATH}"/bzImage \
     -nographic \
     -serial none -device isa-serial,chardev=s1 \
     -chardev stdio,id=s1 \
     -append "panic=-1 notsc" \
-    -initrd "${OUTPUTPATH}"/rootfs.cpio \
+    -initrd "${OUTPUTPATH}"/rootfs-new.cpio \
     -nic user,model=virtio-net-pci \
     -virtfs local,path=${INPUTPATH},mount_tag=host0,security_model=none,id=host0 \
     -virtfs local,path=${OUTPUTPATH},mount_tag=host1,security_model=none,id=host1
