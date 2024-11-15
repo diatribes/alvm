7f46325c581012b1cedee62e81f6730e814ed146
diatribes <diatribes@gmail.com>
2024-11-14 23:37:23 -0500

    simplify and add lightness

--- qrun.sh
+++ qrun.sh
@@ -1,40 +1,22 @@
 #!/bin/bash
 source config.sh
 
-if [ -f "${OUTPUTPATH}/rootfs-new.cpio" ]; then
-    qemu-system-x86_64 \
-        -display none \
-        -no-reboot \
-        -no-user-config \
-        -nodefaults \
-        -cpu host \
-        -enable-kvm \
-        -m 256 \
-        -kernel "${OUTPUTPATH}"/bzImage \
-        -nographic \
-        -serial none -device isa-serial,chardev=s1 \
-        -chardev stdio,id=s1,signal=off \
-        -append "panic=-1 notsc" \
-        -initrd "${OUTPUTPATH}"/rootfs-new.cpio \
-        -nic user,model=virtio-net-pci
-else
-    qemu-system-x86_64 \
-        -display none \
-        -no-reboot \
-        -no-user-config \
-        -nodefaults \
-        -cpu host \
-        -enable-kvm \
-        -m 256 \
-        -kernel "${OUTPUTPATH}"/bzImage \
-        -nographic \
-        -serial none -device isa-serial,chardev=s1 \
-        -chardev stdio,id=s1,signal=off \
-        -append "panic=-1 notsc" \
-        -initrd "${OUTPUTPATH}"/rootfs.cpio \
-        -nic user,model=virtio-net-pci \
-        -virtfs local,path=${INPUTPATH},mount_tag=host0,security_model=none,id=host0 \
-        -virtfs local,path=${OUTPUTPATH},mount_tag=host1,security_model=none,id=host1
-fi
+qemu-system-x86_64 \
+    -display none \
+    -no-reboot \
+    -no-user-config \
+    -nodefaults \
+    -cpu host \
+    -enable-kvm \
+    -m 256 \
+    -kernel "${OUTPUTPATH}"/bzImage \
+    -nographic \
+    -serial none -device isa-serial,chardev=s1 \
+    -chardev stdio,id=s1,signal=off \
+    -append "panic=-1 notsc" \
+    -initrd "${OUTPUTPATH}"/rootfs-new.cpio \
+    -nic user,model=virtio-net-pci \
+    -virtfs local,path=${INPUTPATH},mount_tag=host0,security_model=none,id=host0 \
+    -virtfs local,path=${OUTPUTPATH},mount_tag=host1,security_model=none,id=host1
 
 
